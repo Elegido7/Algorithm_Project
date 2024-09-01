36067c9a0104176a635875467126a5d986a157dd
Dennis <dennojunior@gmail.com>
2024-09-01 16:48:32 +0300

    updated the code and screenshot to match the new update

--- SentenceTest.js
+++ SentenceTest.js
@@ -7,19 +7,26 @@ function analyzeAndRead(sentence) {
   // Define the vowels to be identified in either uppercase and lowercase
   const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
-  //code to Loop through each character in the sentence
+  // Loop through each character in the sentence
   for (let i = 0; i < sentence.length; i++) {
     let char = sentence[i];
     lengthOfSentence++; // Counting the characters in the sentence till the end.
 
-    // Check if the character is a vowel based on the predefined vowels
-    if (vowels.includes(char)) {
-      numberOfVowels++;
-    }
+    // Switch case to handle different types of characters
+    switch (true) {
+      // Check if the character is a vowel
+      case vowels.includes(char):
+        numberOfVowels++;
+        break;
 
-    // Check if the character is a space followed by a non-space (i.e., end of a word)
-    if (char === ' ' && i > 0 && sentence[i - 1] !== ' ') {
-      numberOfWords++;
+      // Check if the character is a space followed by a non-space (i.e., end of a word)
+      case char === ' ' && i > 0 && sentence[i - 1] !== ' ':
+        numberOfWords++;
+        break;
+
+      // Default case to handle other characters (does nothing special here)
+      default:
+        break;
     }
   }
 
@@ -29,11 +36,11 @@ function analyzeAndRead(sentence) {
   }
 
   // Output the results
-  console.log('The Length of the sentence are:', lengthOfSentence);
-  console.log('The Number of words are:', numberOfWords);
-  console.log('The Number of vowels are:', numberOfVowels);
+  console.log('The Length of the sentence is:', lengthOfSentence);
+  console.log('The Number of words is:', numberOfWords);
+  console.log('The Number of vowels is:', numberOfVowels);
 }
 
-//Sentence to test the code
+// Sentence to test the code
 const sentence = 'I will be meeting the CEO of SpaceX, Elon Musk, in the next 24 hours.';
 analyzeAndRead(sentence);
